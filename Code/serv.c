c801e1e10af5d0faff9d27ad56fb81db7d205c85
TantePanique <maxime.doubre@free.fr>
2019-11-23 14:42:17 +0100

    corrections

--- Code/serv.c
+++ Code/serv.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
         printf("problème de création\n");
         exit(0);
     }
-    signal(SIGINT,fin);
+    signal(SIGINT,"Fin");
     
 
     /* bind serveur - socket */
