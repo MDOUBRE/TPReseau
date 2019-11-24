67f1afd4850e8162409099f3d44d0837f83197b8
TantePanique <maxime.doubre@free.fr>
2019-11-24 12:40:39 +0100

    ls ok côté serv, fcts ajout, affich, crée fic/doss

--- CodeOui/serv.c
+++ CodeOui/serv.c
@@ -18,8 +18,11 @@ struct Fichier
 struct Dossier
 {
     char *nom;
-    //struct Dossier[TAILLEMAX];
+    int nbfic;
+    int nbdoss;
+    struct Dossier *tabdossier;
     struct Fichier *tabfichier;
+    //struct Dossier papa;
 };
 
 struct Fichier CreerFichier(char *tab){
@@ -31,31 +34,69 @@ struct Fichier CreerFichier(char *tab){
 struct Dossier CreerDossier(char *tab){
 	struct Dossier dossier;
 	dossier.nom=tab;
+	dossier.nbfic=0;
+	dossier.nbdoss=0;
+	dossier.tabdossier=malloc(sizeof(int)*TAILLEMAX);
 	dossier.tabfichier=malloc(sizeof(int)*TAILLEMAX);
 	return dossier;
 }
 
-
-struct Fichier afficheFichier(struct Fichier fichier){
-	printf("%s\n",fichier.nom);
+//Affiche le contenu du dossier (dossiers et fichiers)
+struct Dossier afficheDossier(struct Dossier dossier){
+	int i=0;
+	printf("%s :\n",dossier.nom);
+	
+	if(dossier.nbfic==0 && dossier.nbdoss==0)
+	{
+		printf("le dossier %s est vide.\n",dossier.nom);
+	}
+	else
+	{	
+		for(i=0;i<dossier.nbdoss;i++)
+		{
+			printf("-%s\n",dossier.tabdossier[i].nom);
+		}
+		for(i=0;i<dossier.nbfic;i++)
+		{
+			printf("-%s\n",dossier.tabfichier[i].nom);
+		}
+	}
 }
 
-struct Dossier afficheDossier(struct Dossier dossier){
-	printf("%s\n",dossier.nom);
-	printf("%s\n",dossier.tabfichier[i].nom);
-	/*
-	for(int i=0;i<TAILLEMAX;i++)
-	{
-		printf("%s\n",dossier.tabfichier[i].nom);
-	}
-	*/
+//fct papa pour le dossier2 qui lui met le dossier1 comme parent quand on crée un dossier2 dans un dossier 1
+
+//Ajoute un fichier dans un dossier et incrémente le nombre de fichiers de la strcucture du dossier
+struct Dossier ajouteDossier(struct Dossier dossier1, struct Dossier dossier2){
+	int i;
+	dossier1.tabdossier[dossier1.nbdoss]=dossier2;
+	dossier1.nbdoss=dossier1.nbdoss+1;
+	return dossier1;
+}
+
+//Ajoute un fichier dans un dossier et incrémente le nombre de fichiers de la strcucture du dossier
+struct Dossier ajouteFichier(struct Dossier dossier, struct Fichier fichier){
+	int i;
+	dossier.tabfichier[dossier.nbfic]=fichier;
+	dossier.nbfic=dossier.nbfic+1;
+	return dossier;
 }
 
 // Fonction de chat entre client et serveur
-void func(int sockfd, struct Dossier *tab) 
+void func(int sockfd) 
 { 
     char buffer[MAX]; 
     int n; 
+    
+    //Création de notre dossier Racine
+    struct Dossier Racine=CreerDossier("Racine");
+    //enlever les /**/ en dessous pour que le ls n'affiche pas "vide"
+    /*
+    struct Fichier fichiertest=CreerFichier("fichiertest");
+    Racine=ajouteFichier(Racine,fichiertest);
+    struct Dossier SousRacine=CreerDossier("SousRacine");
+    Racine=ajouteDossier(Racine,SousRacine);
+    */
+    
     // boucle infinie 
     while (1) { 
         bzero(buffer, MAX); 
@@ -63,7 +104,7 @@ void func(int sockfd, struct Dossier *tab)
         // lit le message du client et le copie dans le buffer 
         read(sockfd, buffer, sizeof(buffer)); 
         // print le buffer qui copntient le contenu du client
-        printf("From client: %s\t To client : ", buffer); 
+        printf("From client: %s\n To client : ", buffer); 
              
         bzero(buffer, MAX); 
         n = 0; 
@@ -74,17 +115,17 @@ void func(int sockfd, struct Dossier *tab)
         // et envoie le buffer au client 
         write(sockfd, buffer, sizeof(buffer));      
 
+        // si le message contient ls alors affiche tous les dossieres et fichiers contenu dans le repertoire
+        if (strncmp("ls", buffer, 2) == 0) { 
+            afficheDossier(Racine);
+        }  
+        
         // si le message contient "Exit"alors le serveur quitte et end la connection 
         if (strncmp("exit", buffer, 4) == 0) { 
             printf("Server Exit...\n"); 
             break; 
         }   
-
-        // si le message contient ls alors affiche tous les dossieres et fichiers contenu dans le repertoire
-        if (strncmp("ls", buffer, 2) == 0) { 
-            printf("repertoires et fichiers...\n"); 
-            break; 
-        }   
+         
     } 
 } 
 
@@ -94,14 +135,20 @@ int main()
     int sockfd, connfd, len; 
     struct sockaddr_in servaddr, cli; 
 
-    //creation d'un dossier test avec un fichier test à l'intérieur
-    struct Dossier *tabDoss=malloc(sizeof(int)*TAILLEMAX);
-    struct Dossier dossiertest=CreerDossier("dossiertestnom");
-    struct Fichier fichiertest=CreerFichier("fichiertestnom");
-    dossiertest.tabfichier[0]=fichiertest;
-    afficheDossier(dossiertest);
-    afficheFichier(fichiertest);
-  
+    //tests dossiers/fichiers/affichage/ajouts
+    /*
+    struct Dossier dossier1=CreerDossier("dossier1");
+    struct Fichier fichier1=CreerFichier("fichier1");
+    struct Fichier fichier2=CreerFichier("fichier2");
+    dossier1=ajouteFichier(dossier1,fichier2);
+    printf("coucou\n\n");
+    afficheDossier(dossier1);
+  	printf("\n\n");
+    dossier1=ajouteFichier(dossier1,fichier1);
+    afficheDossier(dossier1);
+    printf("\n\n");
+    */
+    
     // création et vérification du socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
@@ -144,7 +191,7 @@ int main()
         printf("Le serveur accepte le client...\n"); 
   
     // fonction de chat entre client et serveur 
-    func(connfd,tabDoss); 
+    func(connfd); 
   
     // Fermeture du socket après fin
     close(sockfd); 
